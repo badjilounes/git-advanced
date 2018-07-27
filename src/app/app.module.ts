4a4984ae3cef5258d6295c9390186f2a01f3d8cc
badjilounes <lounes@silk-digital.com>
2018-07-27 14:28:12 +0200

    dashboard component creation via angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { DashbaordComponent } from './dashbaord/dashbaord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    DashbaordComponent
   ],
   imports: [
     BrowserModule,
