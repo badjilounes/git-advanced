63841b309ac3194bf26ab6966caec376a1a47bd4
badjilounes <lounes@silk-digital.com>
2018-07-27 14:14:37 +0200

    list component creation via angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashbaordComponent
+    DashbaordComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
